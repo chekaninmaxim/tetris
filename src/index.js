78562d0b8426a41b307862880c1bd8c57098d05b
chekanin <chekaninm@gmail.com>
2021-01-09 13:33:27 +0200

    Path fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import './index.css';
-import App from './components/App';
+import App from './components/game/Game';
 
 ReactDOM.render(
   <Provider store={ store } >
